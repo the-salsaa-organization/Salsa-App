26a2c64bc2053001677c3e5ecc1a38c3ff73786d
Kevin Pozzi <kevindavidpozzi@gmail.com>
2019-03-28 18:25:42 -0700

    implemented CSS modules

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
-import './App.css';
+import styles from './App.module.css';
+import Recipe from './Recipe.js';
+import Header from './Header.js';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      PageType: 'Recipe',
+    }
+    this.displayRecipe = this.displayRecipe.bind(this);
+  }
+  displayRecipe() {
+    if (this.state.PageType === 'Recipe') {
+      this.setState({
+        PageType: 'Nothing'
+      })
+    } else {
+      this.setState({
+        PageType: 'Recipe'
+      })
+    }
+  }
   render() {
     return (
-      <div className="App">
-        Hi
+      
+      <div className={styles.App}>
+        <Header currentPage = {this.state.PageType}/>
+        <button onClick = {this.displayRecipe}>showRecipe</button>
+        {this.state.PageType === 'Recipe' ? <Recipe/> : null}
+        {this.state.PageType === 'Nothing' ? <p>fuck nick</p> : null}
       </div>
     );
   }
---
+++ src/Header.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import styles from './Header.module.css';
+
+class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      
+    }
+  }
+  render() {
+    return (
+      <div className={`${styles.Header} ${styles[this.props.currentPage]}`}>
+        <p>header</p>
+      </div>
+    );
+  }
+}
+
+export default Header;
